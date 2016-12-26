a69b3d5696c6cbe46a8da6d842bcac9a43ecf9b8
github username <github email address>
2016-12-26 15:57:15 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :students, only: :index
+  resources :students, only: [:index, :new, :create]   
   get '/student/:id', to: 'students#show', as: 'student'
 end
+
+
+# do first
+#create a new.html.erb file 
+# in controller do a new and create method
